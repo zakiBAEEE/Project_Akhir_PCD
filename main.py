3cfe81112f1f08e1fcaa73b1b95fcb00af056c1f
Muhammad Dzaky Raihan <muhammaddzakyraihan_2226250100@mhs.mdp.ac.id>
2024-06-10 20:58:42 +0700

    Menambahkan script.js

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
 
-import os
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
-
-# Tentukan direktori tempat gambar akan disimpan
-UPLOAD_FOLDER = 'uploads'
-# Pastikan direktori tersebut ada, jika tidak, buat direktori baru
-if not os.path.exists(UPLOAD_FOLDER):
-    os.makedirs(UPLOAD_FOLDER)
-
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def index():
@@ -20,14 +11,8 @@ def index():
 def upload():
     if 'image' not in request.files:
         return 'No file part'
-    file = request.files['image']
-    if file.filename == '':
-        return 'No selected file'
-    if file:
-        filename = file.filename
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        file.save(file_path)
-        return render_template('upload_succes.html', filename=filename)
+    file = request.form.get('image')
+    return render_template('upload_success.html', fileGambar = file)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- templates/script.js
+++
@@ -1,9 +0,0 @@
-function previewImage(event) {
-    var reader = new FileReader();
-    reader.onload = function() {
-        var preview = document.getElementById('preview');
-        preview.src = reader.result;
-        preview.style.display = 'block';
-    }
-    reader.readAsDataURL(event.target.files[0]);
-}
