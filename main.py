a6f76f0f8bbf1cd406d7632fc8cf0b9fc6476f1e
Muhammad Dzaky Raihan <muhammaddzakyraihan_2226250100@mhs.mdp.ac.id>
2024-06-11 09:00:33 +0700

    Add __init__.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-
 from flask import Flask, request, render_template
+from image_processing.process import process_image
 
 app = Flask(__name__)
 
@@ -10,8 +10,8 @@ def index():
 
 @app.route('/upload', methods=['GET','POST'] )
 def upload():
-    file = request.files.get('image')
-    return render_template('index.html', fileGambar=file)
+ file = request.files.get('image')
+ return render_template('index.html', fileGambar=file)
 
 if __name__ == '__main__':
     app.run(debug=True)
