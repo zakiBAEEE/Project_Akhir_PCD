758504664b21182ead5ac46261d9634c9ca9c274
Muhammad Dzaky Raihan <muhammaddzakyraihan_2226250100@mhs.mdp.ac.id>
2024-06-11 08:31:46 +0700

    Menerapkan Jinja Template

--- main.py
+++ main.py
@@ -7,12 +7,11 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/upload', methods=['POST'])
+
+@app.route('/upload', methods=['GET','POST'] )
 def upload():
-    if 'image' not in request.files:
-        return 'No file part'
-    file = request.form.get('image')
-    return render_template('upload_success.html', fileGambar = file)
+    file = request.files.get('image')
+    return render_template('index.html', fileGambar=file)
 
 if __name__ == '__main__':
     app.run(debug=True)
